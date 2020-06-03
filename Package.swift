be6f5985ae6da510f0b64474689cd38b3e7e3874
Christopher Prince <chris@SpasticMuffin.biz>
2020-06-02 20:45:55 -0600

    Fixed package issue.

--- Package.swift
+++ Package.swift
@@ -12,15 +12,15 @@ let package = Package(
             targets: ["ServerShared"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.7.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect.git", from: "3.1.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ServerShared",
-            dependencies: []),
+            dependencies: ["Kitura", "PerfectLib"]),
         .testTarget(
             name: "ServerSharedTests",
             dependencies: ["ServerShared"]),
