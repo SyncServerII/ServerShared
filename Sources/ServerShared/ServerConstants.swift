76f712b22dad879355edf7199cd6787ec9392093
Christopher Prince <chris@SpasticMuffin.biz>
2020-06-02 22:02:05 -0600

    Update constants.

--- Sources/ServerShared/ServerConstants.swift
+++ Sources/ServerShared/ServerConstants.swift
@@ -1,27 +1,55 @@
+// These are shared with client apps
+
 public class ServerConstants {
     // Generic HTTP request header authentication keys; the values for these keys are duplicated from Kitura (they didn't give named constants).
     public static let XTokenTypeKey = "X-token-type"
-    
     public static let HTTPOAuth2AccessTokenKey = "access_token"
-    
-    // Necessary for some authorization systems, e.g., Dropbox.
-    public static let HTTPAccountIdKey = "X-account-id"
     
     // HTTP request header keys specific to Google
     @available(*, deprecated, message: "Use: HTTPOAuth2AuthorizationCodeKey")
     public static let GoogleHTTPServerAuthCodeKey = "SyncServer-Google-server-auth-code"
+
+    // OAuth2 authorization code, e.g., from Google
+    public static let HTTPOAuth2AuthorizationCodeKey = "SyncServer-authorization-code"
+    
+    /// Necessary for some authorization systems, e.g., Dropbox.
+    /// DEPRECATED: Use: HTTPAccountDetailsKey
+    public static let HTTPAccountIdKey = "X-account-id"
+
+    /// Used by Apple Sign In. Should convert Dropbox over to using this too.
+    /// Content is a JSON structure.
+    public static let HTTPAccountDetailsKey = "X-account-details"
+
+    // Microsoft has something, from iOS MSAL, they call an access token, but it's not a JWT.
+    public static let HTTPMicrosoftAccessToken = "X-microsoft-access-token"
+
+#if DEBUG
+    // Give this key any string value to test failing of an endpoint.
+    public static let httpRequestEndpointFailureTestKey = "SyncServer-FailureTest"
+#endif
     
     // HTTP: request header key
     // Since the Device-UUID is a somewhat secure identifier, I'm passing it in the HTTP header. Plus, it makes the device UUID available early in request processing.
     public static let httpRequestDeviceUUID = "SyncServer-Device-UUID"
     
+    // HTTP response header keys
+    // 9/9/17; Keep these header keys in *lower case* to be compatible with NGNIX-- which sends them back in lower case.
+    
+    // Used when downloading a file to return parameters (as a HTTP header response header).
+    public static let httpResponseMessageParams = "syncserver-message-params"
+
     // Used for some Account types (e.g., Facebook)
     public static let httpResponseOAuth2AccessTokenKey = "syncserver-access-token"
     
     // The value of this key is a "X.Y.Z" version string.
     public static let httpResponseCurrentServerVersion = "syncserver-version"
+
+    // If present, the value of this key is a "X.Y.Z" version string. This is intended to be the minimum version of the *client* app not the SyncServer iOS client interface (i.e., not https://github.com/crspybits/SyncServer-iOSClient).
+    public static let httpResponseMinimumIOSClientAppVersion = "syncserver-minimum-ios-client-app-version"
     
-    // Used when downloading a file to return parameters (as a HTTP header response header).
-    public static let httpResponseMessageParams = "syncserver-message-params"
+    public static let maxNumberSharingInvitationAcceptors:UInt = 10
+    
+    // 60 seconds/minute * 60 minutes/hour * 24 hours/day == seconds/day
+    public static let sharingInvitationExpiryDuration:TimeInterval = 60*60*24 // 1 day
 }
 
